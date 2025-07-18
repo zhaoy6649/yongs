312663e43641f45becb3e6a885e2b8f374d1b3b0
zhaoy6649 <119747675+zhaoy6649@users.noreply.github.com>
2025-07-18 08:13:39 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -114,48 +114,39 @@ const API_SITES = {
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
-        adult: true
-    },
+      },
      ckzy: {
          api: 'https://www.ckzy1.com/api.php/provide/vod',
          name: 'CK资源',
-         adult: true
      },
      jkun: {
          api: 'https://jkunzyapi.com/api.php/provide/vod',
          name: 'jkun资源',
-         adult: true
      },
      bwzy: {
          api: 'https://api.bwzym3u8.com/api.php/provide/vod',
          name: '百万资源',
-         adult: true
      },
      souav: {
          api: 'https://api.souavzy.vip/api.php/provide/vod',
          name: 'souav资源',
-         adult: true
      },
      r155: {
          api: 'https://155api.com/api.php/provide/vod',
          name: '155资源',
-         adult: true
      },
      lsb: {
          api: 'https://apilsbzy1.com/api.php/provide/vod',
          name: 'lsb资源',
-         adult: true
      },
      huangcang: {
          api: 'https://hsckzy.vip/api.php/provide/vod',
          name: '黄色仓库',
-         adult: true,
          detail: 'https://hsckzy.vip/api.php/provide/vod'
      },
      yutu: {
          api: 'https://yutuzy10.com/api.php/provide/vod',
          name: '玉兔资源',
-         adult: true
      },
 
     // 下面是资源失效率高的API源，不建议使用
